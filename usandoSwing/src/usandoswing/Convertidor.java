a30448a6d911a7d1a5bccc097ae808f7b59931f3
Sebas <seplarui@gmail.com>
2018-03-09 12:37:26 +0100

    Convertidor Fin

--- usandoSwing/src/usandoswing/Convertidor.java
+++ usandoSwing/src/usandoswing/Convertidor.java
@@ -1,8 +1,8 @@
 package usandoswing;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,6 +15,11 @@ import javax.swing.JTextField;
  */
 public class Convertidor extends JFrame {
 
+    JPanel panel = new JPanel();
+    JTextField kelvin = new JTextField("Grados Kelvin", 5);
+    JTextField celsius = new JTextField("Grados Celsius", 5);
+
+    
     public Convertidor() {
 
         setTitle("Convertidor Grados");
@@ -22,24 +27,50 @@ public class Convertidor extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
-        GridLayout grid= new GridLayout(4,1,5,5);
+        GridLayout grid = new GridLayout(4, 1, 5, 5);
         JLabel etiqueta1 = new JLabel("Grados Celsius:");
         JLabel etiqueta2 = new JLabel("Grados Kelvin:");
-        JTextField kelvin = new JTextField("Grados Kelvin");
-        JTextField celsius = new JTextField("Grados Celsius");
+
+        celsius.addActionListener(new ConvertirGrados());
         JButton limpiar = new JButton("Limpiar");
+        limpiar.addActionListener(new LimpiarTextos());
+
         Dimension d = new Dimension();
         d.height = 40;
         d.width = 40;
         limpiar.setPreferredSize(d);
         panel.setLayout(grid);
         panel.add(etiqueta1);
-        panel.add(kelvin);
-
-        panel.add(etiqueta2);
         panel.add(celsius);
+        panel.add(etiqueta2);
+        panel.add(kelvin);
         panel.add(limpiar);
         add(panel);
+
+    }
+    
+    class ConvertirGrados implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String grado= celsius.getText();
+            
+            int convertido= Integer.parseInt(grado);
+            convertido= convertido + 273;
+            
+          String  resultado= String.valueOf(convertido);
+
+            kelvin.setText(resultado);
+            
+        }
+    }
+
+    class LimpiarTextos implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+
+            kelvin.setText("");
+            celsius.setText("");
+
+        }
 
     }
 
