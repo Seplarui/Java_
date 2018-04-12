4232fd2ce900cf6f67c658cf09357967fd781c4e
Sebas <seplarui@gmail.com>
2018-04-12 14:15:59 +0200

    Primitiva

---
+++ Primitiva/src/java/util/Aleatorio.java
@@ -0,0 +1,18 @@
+package java.util;
+
+/**
+ *
+ * @author haplo
+ */
+public class Aleatorio extends Random {
+    
+    public int nextInt(int inferior, int superior) {
+        int i;
+        i=nextInt();
+        i=inferior+(Math.abs(i) %(superior-inferior+1));
+        return i;
+    }
+    
+    
+    
+}
---
+++ Primitiva/src/java/util/Primitiva.java
@@ -0,0 +1,17 @@
+package java.util;
+
+
+/**
+ *
+ * @author haplo
+ */
+public class Primitiva {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
