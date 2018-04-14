7c6461fa8bfcece24fabab7dba6c21152ad380a5
Sebas <seplarui@gmail.com>
2018-04-14 16:23:51 +0200

    Conexion_DB.java

--- pruebaMySQL/src/DAO/Conexion_DB.java
+++ pruebaMySQL/src/DAO/Conexion_DB.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DAO;
+import java.sql.*;
 
-/**
- *
- * @author haplo
- */
 public class Conexion_DB {
+    public Connection AbrirConexion() throws Exception {
+        
+        Connection con=null; //instancia una conexión
+        
+        try{
+            String urlOdbc="jdbc:mysql://localhost:3306/di";
+            
+            //crea conexión. 
+            
+            con=(java.sql.DriverManager.getConnection(urlOdbc,"root",""));
+            return con;
+        } catch(Exception e) {
+            e.printStackTrace();
+            throw new Exception("Ha sido imposible establecer la conexión"+ e.getMessage());
+            
+        }
+    }
+    
+    public void CerrarConexion(Connection con) throws Exception {
+        
+        try {
+            if(con!=null) 
+                con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new Exception("Ha sido imposible cerrar la conexión"+e.getMessage());
+        }
+    }
     
 }
