6f252e229a2dfa6bcef27cefe657c8ad42f358df
Sebas <seplarui@gmail.com>
2018-04-14 17:15:44 +0200

    mySQL

--- pruebaMySQL/src/pruebamysql/PruebaMySQL.java
+++ pruebaMySQL/src/pruebamysql/PruebaMySQL.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pruebamysql;
 
-/**
- *
- * @author haplo
- */
+import ui.frame;
+
 public class PruebaMySQL {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        frame f = new frame();
+        f.setVisible(true);
     }
-    
+
 }
--- pruebaMySQL/src/ui/listener.java
+++ pruebaMySQL/src/ui/listener.java
@@ -1,14 +1,99 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ui;
 
-/**
- *
- * @author haplo
- */
-public class listener {
+import DAO.ClienteDAO;
+import DAO.Conexion_DB;
+import DAO.cliente;
+
+import java.awt.event.*;
+import java.sql.*;
+import java.util.List;
+
+import javax.swing.*;
+
+public class listener implements ActionListener {
+
+    private JTextField id, idBuscar;
+    private JTextArea notas;
+    private JButton atras,adelante,buscar,todos;
+    private List<cliente> clientes;
     
+    private int posicion;
+    
+    public listener(JTextField id, JTextArea notas, JTextField idBuscar, JButton atras, JButton adelante, JButton buscar, JButton todos) {
+        this.id=id;
+        this.notas=notas;
+        this.idBuscar=idBuscar;
+        this.atras=atras;
+        this.adelante=adelante;
+        this.buscar=buscar;
+        this.todos=todos;
+        posicion=0;
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        cliente c= new cliente();
+        
+        if(e.getSource()==this.buscar) {
+            adelante.setEnabled(false);
+            atras.setEnabled(false);
+            
+            try {
+                Conexion_DB conexion_DB= new Conexion_DB();
+                Connection con= conexion_DB.AbrirConexion();
+                
+                ClienteDAO clienteDAO= new ClienteDAO();
+                c.setId(idBuscar.getText());
+                c=clienteDAO.findById(con, c);
+                conexion_DB.CerrarConexion(con);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        
+        if(e.getSource()==this.todos) {
+            adelante.setEnabled(true);
+            atras.setEnabled(true);
+            
+            posicion=0;
+            
+            try{
+                Conexion_DB conexion_DB=new Conexion_DB();
+                Connection con= conexion_DB.AbrirConexion();
+                ClienteDAO clienteDAO= new ClienteDAO();
+                
+                clientes=clienteDAO.findAll(con);
+                conexion_DB.CerrarConexion(con);
+                
+                posicion=0;
+                c=clientes.get(posicion);
+                
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        
+        if(e.getSource()==this.adelante) {
+            posicion++;
+            if(posicion==clientes.size()) {
+                posicion--;
+            }
+            c=clientes.get(posicion);
+        }
+        
+        //ATRAS
+        
+        if(e.getSource()==this.atras) {
+            if(posicion>0) {
+                posicion--;
+            }
+            
+            c=clientes.get(posicion);
+        }
+        actualiza(c);
+    }
+    
+    private void actualiza(cliente c) {
+        this.id.setText(c.getId());
+        this.notas.setText(c.getNotas());
+    }
 }
