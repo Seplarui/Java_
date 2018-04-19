ce0f326a1e19c9414630e0347e2581674689c75e
Sebas <seplarui@gmail.com>
2018-04-19 10:31:26 +0100

    Evento boton

--- usandoSwing/src/usandoswing/frame2.java
+++ usandoSwing/src/usandoswing/frame2.java
@@ -3,9 +3,13 @@ package usandoswing;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class frame2 extends JFrame{
+    
+    private JPanel panel= new JPanel();
     
     public frame2() {
         
@@ -13,19 +17,25 @@ public class frame2 extends JFrame{
     setTitle("Usando Swing");
     setSize(500,500);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    JPanel panel= new JPanel();
     
     add(panel);
     
     panel.setBackground(Color.red);
     JButton boton= new JButton("Boton");
+    boton.addActionListener(new escuchador_boton());
     Dimension d= new Dimension();
     
     d.height=80;
     d.width=200;
     boton.setPreferredSize(d);
     panel.add(boton);
-    
-    
     }
+    
+    class escuchador_boton implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            panel.setBackground(Color.blue);
+        }
+    }
+    
+
 }
